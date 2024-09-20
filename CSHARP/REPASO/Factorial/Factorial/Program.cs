d13e6e0ca0a0ab3a80e7ea79bbd1513d01d33158
Leiker Castillo <leikercg@gmail.com>
2024-09-20 13:42:47 +0200

    Primer commit

---
+++ CSHARP/REPASO/Factorial/Factorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaro las variables
+            int i, n, fact;
+            Console.WriteLine("PROGRAMA FACTORIAL");
+            Console.WriteLine();
+            Console.Write("Introduce un numero para calcular su factorial: ");
+
+            //INICIALIZAR
+            n = int.Parse(Console.ReadLine());
+            fact = 1;
+
+            //INICIANALIZO LA SECUENCIA
+            i = 1;
+
+            //RECORRO LA SECUENCIA CON UN BUCLE
+            while (i <= n)
+            {
+                //TRATO EL ELEMENTO DE LA SECUENCIA
+                fact = fact * i;
+                // INREMENTO LA SECUENCIA
+                i++;
+            }
+            //ESCRIBO EL RESULTADO
+            Console.WriteLine($"El factorial de {n} es: {fact}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSHARP/REPASO/Hola/Hola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio = 2.5;
+
+            double resultado = radio * radio * Math.PI;
+
+            decimal resultadoDecimal= (decimal)(radio * radio * Math.PI);
+
+            Console.WriteLine("El area es "+ resultado);
+            Console.WriteLine("El area con decimal es " + resultado);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
