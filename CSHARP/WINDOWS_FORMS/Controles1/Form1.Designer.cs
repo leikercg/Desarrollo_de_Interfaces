25cc6af7a0644939da6a8250c7357237c0ed2f8f
Leiker Castillo <leikercg@gmail.com>
2024-11-15 11:50:37 +0100

    Forms en C#

---
+++ CSHARP/WINDOWS_FORMS/Controles1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Controles1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_saludar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.BackColor = System.Drawing.Color.Orange;
+            this.btn_Cerrar.Location = new System.Drawing.Point(615, 364);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(105, 48);
+            this.btn_Cerrar.TabIndex = 0;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
+            this.btn_Cerrar.Click += new System.EventHandler(this.click_cerrar);
+            this.btn_Cerrar.MouseEnter += new System.EventHandler(this.entrar_btn);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(205, 85);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(153, 20);
+            this.nombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introduce tu nombre";
+            // 
+            // btn_saludar
+            // 
+            this.btn_saludar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_saludar.Location = new System.Drawing.Point(100, 364);
+            this.btn_saludar.Name = "btn_saludar";
+            this.btn_saludar.Size = new System.Drawing.Size(105, 48);
+            this.btn_saludar.TabIndex = 3;
+            this.btn_saludar.Text = "Saludar";
+            this.btn_saludar.UseVisualStyleBackColor = false;
+            this.btn_saludar.Click += new System.EventHandler(this.btn_saludar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(322, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hola";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_saludar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Name = "Form1";
+            this.Text = "Controles";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_saludar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CSHARP/WINDOWS_FORMS/Controles1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controles1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Fooooooooooormulario";
+            btn_Cerrar.Text = "Formulario cerrado";
+        }
+
+        private void click_cerrar(object sender, EventArgs e)
+        {
+            this.Close(); // Cerrar el formulario con this, no referimos al form
+        }
+
+        private void entrar_btn(object sender, EventArgs e)
+        {
+            btn_Cerrar.BackColor = Color.DarkRed;
+        }
+
+        private void btn_saludar_Click(object sender, EventArgs e)
+        {
+            if (nombre.Text == "")
+            {
+                label2.Text = "Introduce un nombre";
+                label2.ForeColor = Color.Red;  // Cambiar el color del texto a rojo
+            }
+            else
+            {
+                label2.Text = label2.Text + " " + nombre.Text;
+            }
+        }
+
+     
+    }
+}   
