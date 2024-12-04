f494e3d10dc63a82f357a0dc48f1f7963fc7d778
Leiker Castillo <leikercg@gmail.com>
2024-12-04 13:27:13 +0100

    picture

---
+++ CSHARP/WINDOWS_FORMS/WF_pictureBox/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WF_pictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonRojo = new System.Windows.Forms.Button();
+            this.buttonAzul = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(47, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 184);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WF_pictureBox.Properties.Resources.verde;
+            this.pictureBox2.Location = new System.Drawing.Point(262, 99);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(148, 119);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonRojo
+            // 
+            this.buttonRojo.Location = new System.Drawing.Point(281, 241);
+            this.buttonRojo.Name = "buttonRojo";
+            this.buttonRojo.Size = new System.Drawing.Size(75, 23);
+            this.buttonRojo.TabIndex = 2;
+            this.buttonRojo.Text = "ROJO";
+            this.buttonRojo.UseVisualStyleBackColor = true;
+            this.buttonRojo.Click += new System.EventHandler(this.buttonRojo_Click);
+            // 
+            // buttonAzul
+            // 
+            this.buttonAzul.Location = new System.Drawing.Point(281, 289);
+            this.buttonAzul.Name = "buttonAzul";
+            this.buttonAzul.Size = new System.Drawing.Size(75, 23);
+            this.buttonAzul.TabIndex = 3;
+            this.buttonAzul.Text = "AZUL";
+            this.buttonAzul.UseVisualStyleBackColor = true;
+            this.buttonAzul.Click += new System.EventHandler(this.buttonAzul_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(490, 241);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(465, 99);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(134, 119);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonAzul);
+            this.Controls.Add(this.buttonRojo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Trabajando con imágenes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonRojo;
+        private System.Windows.Forms.Button buttonAzul;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ CSHARP/WINDOWS_FORMS/WF_pictureBox/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_pictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRojo_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Properties.Resources.rojo;
+        }
+
+        private void buttonAzul_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Properties.Resources.azul;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo directorio = new DirectoryInfo("COLORES");
+            FileInfo[] archivos = directorio.GetFiles("*.png");
+            for (int i = 0; i < archivos.Length; i++)
+            {
+                listBox1.Items.Add(archivos[i].Name); // Añadir el nombre completo al listBox
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) // EL elemento selecionado
+        {
+            pictureBox3.Image = Image.FromFile("colores\\"+listBox1.SelectedItem.ToString());
+
+        }
+    }
+}
