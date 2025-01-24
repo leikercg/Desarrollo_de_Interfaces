0ad5a0b7fd094190041270b529b2265bffa3a9a4
Leiker Castillo <leikercg@gmail.com>
2025-01-24 11:09:17 +0100

    WPF WORDS

---
+++ CSHARP/WPF/WPF_ITEMS_ CONTROLLS/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ITEMS__CONTROLLS //ojo, doble guión
+{
+    internal class Cliente
+    {
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; } 
+        public decimal ImporteTotalCompras { get; set; }
+    }
+}
