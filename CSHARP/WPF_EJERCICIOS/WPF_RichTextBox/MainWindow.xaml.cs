608cd7069a0038cec7cdff78d148f29289db4933
Leiker Castillo <leikercg@gmail.com>
2025-02-11 11:49:22 +0100

    Todos los ejercicios

---
+++ CSHARP/WPF_EJERCICIOS/WPF_RichTextBox/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_RichTextBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNegrita_Click(object sender, RoutedEventArgs e)
+        {
+            zonaTexto.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void btnCursiva_Click(object sender, RoutedEventArgs e)
+        {
+            zonaTexto.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+        }
+    }
+}
